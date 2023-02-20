a73f39db9f9a09836d70dd63c692b6678ba020c2
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2023-02-20 10:35:50 +0800

    Convert config

--- app.config.js
+++ app.config.js
@@ -1,36 +1,34 @@
-{
-  "expo": {
-    "name": "expo-mre",
-    "slug": "expo-mre",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "myapp",
-    "userInterfaceStyle": "automatic",
-    "splash": {
-      "image": "./assets/images/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+module.exports = {
+  name: "expo-mre",
+  slug: "expo-mre",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: "./assets/images/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.bndkt.expo-mre",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "com.bndkt.expo-mre"
+  },
+  web: {
+    bundler: "metro",
+    favicon: "./assets/images/favicon.png",
+  },
+  extra: {
+    eas: {
+      projectId: "94c985f0-8e87-4924-8618-0876965db3b4",
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
-      }
-    },
-    "web": {
-      "bundler": "metro",
-      "favicon": "./assets/images/favicon.png"
-    },
-    "extra": {
-      "eas": {
-        "projectId": "94c985f0-8e87-4924-8618-0876965db3b4"
-      }
-    },
-    "owner": "mueller"
-  }
-}
+  },
+  owner: "mueller",
+};
